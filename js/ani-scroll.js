b6a9916c55cbaddaff5bf7b76ae601a0ba1dd9d4
Matt Hogeboom <matthogeboom@gmail.com>
2021-07-03 11:17:42 -0700

    11.6 - mobile navigation

--- js/ani-scroll.js
+++ js/ani-scroll.js
@@ -3,3 +3,12 @@
 AOS.init({
   duration: 1200,
 })
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,4 +4,13 @@ if($(window).width() > 1200) {
 
 
   }
+
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
   
